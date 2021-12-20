54ef25fe9d179960be7e9b61066d6e923199db38
VirgilClyne <Virgil@nanocat.me>
2021-12-20 11:48:37 +0800

    Update Netflix.js
    Update Netflix.js
    
    Update Netflix.js
    
    Update Netflix.js

--- js/Netflix.js
+++ js/Netflix.js
@@ -9,23 +9,35 @@ const isQuanX = typeof $task != "undefined";
 const isLoon = typeof $loon != "undefined";
 // 关闭请求
 const done = (value = {}) => {
-    if (isQuanX) return $done(value);
-    if (isSurge) isRequest ? $done(value) : $done();
+	if (isQuanX) return $done(value);
+	if (isSurge) isRequest ? $done(value) : $done();
 };
 
 /*
 README:https://github.com/VirgilClyne/iRingo
 */
 
+// Default Settings
+geolocation = {};
+geolocation.policy = "ALLOW";
+geolocation.country = "SG"
+allowWidevinePlayback = true;
+airPlayDisabledEnabledOnBuild = "0";
+//preferRichWebVTTOverImageBasedSubtitle = true;
+reuseAVPlayerEnabledOnBuild = "0";
+nfplayerReduxEnabledOnBuild = "0";
+
 // Argument Function Supported
 if (typeof $argument != "undefined") {
-    let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
-    console.log(JSON.stringify(arg));
-    geolocationPolicy = (arg.geolocation.policy!= "undefined") ? arg.geolocation.policy : "ALLOW";
-    geolocationCountry = (arg.geolocation.country!= "undefined") ? arg.geolocation.country : "SG";
-    allowWidevinePlayback = (arg.allowWidevinePlayback!= "undefined") ? arg.allowWidevinePlayback : true;
-    airPlayDisabledEnabledOnBuild = (arg.airPlayDisabledEnabledOnBuild!= "undefined") ? arg.airPlayDisabledEnabledOnBuild : "99.99.0";
-    preferRichWebVTTOverImageBasedSubtitle = (arg.preferRichWebVTTOverImageBasedSubtitle!= "undefined") ? arg.preferRichWebVTTOverImageBasedSubtitle : true;
+	let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+	console.log(JSON.stringify(arg));
+	geolocation.policy = arg.geolocation.policy
+	geolocation.country = arg.geolocation.country;
+	allowWidevinePlayback = arg.allowWidevinePlayback;
+	airPlayDisabledEnabledOnBuild = arg.airPlayDisabledEnabledOnBuild;
+	//preferRichWebVTTOverImageBasedSubtitle = arg.preferRichWebVTTOverImageBasedSubtitle;
+	reuseAVPlayerEnabledOnBuild = arg.reuseAVPlayerEnabledOnBuild;
+	nfplayerReduxEnabledOnBuild = arg.nfplayerReduxEnabledOnBuild;
 };
 
 const url = $request.url;
@@ -33,14 +45,15 @@ const url = $request.url;
 const path1 = "/iosui/user/";
 
 if (url.search(path1) != -1) {
-  let body = $response.body;
-  console.log(path1);
-  let content = JSON.parse(body);
-  if (content.value?.geolocation?.policy) content.value.geolocation.policy = geolocationPolicy
-  if (content.value?.geolocation?.country) content.value.geolocation.country = geolocationCountry;
-  if (content.value?.config?.allowWidevinePlayback) content.value.config.allowWidevinePlayback = allowWidevinePlayback;
-  if (content.value?.config?.airPlayDisabledEnabledOnBuild) content.value.config.airPlayDisabledEnabledOnBuild = airPlayDisabledEnabledOnBuild;
-  if (content.value?.config?.preferRichWebVTTOverImageBasedSubtitle) content.value.config.preferRichWebVTTOverImageBasedSubtitle = preferRichWebVTTOverImageBasedSubtitle;
-  body = JSON.stringify(content);
-  done({ body });
+	let body = $response.body;
+	console.log(path1);
+	let content = JSON.parse(body);
+	if (content.value?.geolocation?.policy) content.value.geolocation.policy = geolocation.policy
+	if (content.value?.geolocation?.country) content.value.geolocation.country = geolocation.country;
+	if (content.value?.config?.allowWidevinePlayback) content.value.config.allowWidevinePlayback = allowWidevinePlayback;
+	if (content.value?.config?.airPlayDisabledEnabledOnBuild) content.value.config.airPlayDisabledEnabledOnBuild = airPlayDisabledEnabledOnBuild;
+	//if (content.value?.config?.preferRichWebVTTOverImageBasedSubtitle) content.value.config.preferRichWebVTTOverImageBasedSubtitle = preferRichWebVTTOverImageBasedSubtitle;
+	if (content.value?.config?.nfplayerReduxEnabledOnBuild) content.value.config.nfplayerReduxEnabledOnBuild = nfplayerReduxEnabledOnBuild;
+	body = JSON.stringify(content);
+	done({ body });
 };
