890280200c9c6552ff1017d02037ae647259b835
VirgilClyne <Virgil@nanocat.me>
2022-01-17 10:18:20 +0800

    Update Netflix.js

--- js/Netflix.js
+++ js/Netflix.js
@@ -18,46 +18,90 @@ README:https://github.com/VirgilClyne/GetSomeFries
 */
 
 // Default Settings
-var geolocation = {};
+var geolocation = {
+	"policy": "ALLOW", //策略
+	"country": "SG", // 国家
+};
+console.log(geolocation);
+/*
 geolocation.policy = "ALLOW";
 geolocation.country = "SG"
-var config = {};
+*/
+var config = {
+	"allowWidevinePlayback": true, // 允许Widevine DRM回放
+	"airPlayDisabledEnabledOnBuild": "50.0.0", // 开始禁用airPlay的版本
+	"preferRichWebVTTOverImageBasedSubtitle": true, // 偏好使用RichWebVTT字幕多于图片字幕
+	"reuseAVPlayerEnabledOnBuild": "0", // 重新开始启用AVPlayer的版本
+	"nfplayerReduxEnabledOnBuild": "50.0.0", // 开始启用nfplayerRedux的版本
+};
+console.log(config);
+/*
 config.allowWidevinePlayback = true;
 config.airPlayDisabledEnabledOnBuild = "50.0.0";
 config.preferRichWebVTTOverImageBasedSubtitle = true;
 config.reuseAVPlayerEnabledOnBuild = "0";
 config.nfplayerReduxEnabledOnBuild = "50.0.0";
+*/
 
+var ctx = {
+	"region": "", // 当前IP所属地区
+	//"monotonic": true, // 函数？
+	//"device": "", // 当前使用设备
+	"isolate_pool": true, // 隔离池？
+	//"iter": 0,
+	//"abtests":55, // AB测试
+	//"ts":1642392069933, // timestamp？
+	"ip": "",
+	"hasUser": false // 当前IP是否有用户
+};
+console.log(ctx);
+
+/*
 // Argument Function Supported
 if (typeof $argument != "undefined") {
 	let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
 	console.log(JSON.stringify(arg));
-	geolocation.policy = arg.geolocation.policy
-	geolocation.country = arg.geolocation.country;
-	config.allowWidevinePlayback = Boolean(JSON.parse(arg.config.allowWidevinePlayback));
-	config.airPlayDisabledEnabledOnBuild = arg.config.airPlayDisabledEnabledOnBuild;
-	config.preferRichWebVTTOverImageBasedSubtitle = Boolean(JSON.parse(arg.preferRichWebVTTOverImageBasedSubtitle));
-	config.reuseAVPlayerEnabledOnBuild = arg.config.reuseAVPlayerEnabledOnBuild;
-	config.nfplayerReduxEnabledOnBuild = arg.config.nfplayerReduxEnabledOnBuild;
+	geolocation.policy = arg.geolocation_policy
+	geolocation.country = arg.geolocation_country;
+	config.allowWidevinePlayback = Boolean(JSON.parse(arg.config_allowWidevinePlayback));
+	config.airPlayDisabledEnabledOnBuild = arg.config_airPlayDisabledEnabledOnBuild;
+	config.preferRichWebVTTOverImageBasedSubtitle = Boolean(JSON.parse(arg.config_preferRichWebVTTOverImageBasedSubtitle));
+	config.reuseAVPlayerEnabledOnBuild = arg.config_reuseAVPlayerEnabledOnBuild;
+	config.nfplayerReduxEnabledOnBuild = arg.config_nfplayerReduxEnabledOnBuild;
 };
+*/
 
 const url = $request.url;
+var body = $response.body;
 
-const path1 = "/iosui/user/";
+const path1 = "/iosui/user";
+const path2 = "/ftl/probe";
 
+//Function Settings
 if (url.search(path1) != -1) {
-	let body = $response.body;
-	console.log(path1);
+	console.log(url);
 	let content = JSON.parse(body);
-	console.log(content.value.config.allowWidevinePlayback.toString());
-	console.log(content.value.config.allowWidevinePlayback.valueOf());
-	if (content.value?.geolocation?.policy) content.value.geolocation.policy = geolocation.policy
-	if (content.value?.geolocation?.country) content.value.geolocation.country = geolocation.country;
-	if (content.value?.config?.allowWidevinePlayback) content.value.config.allowWidevinePlayback = config.allowWidevinePlayback;
-	if (content.value?.config?.airPlayDisabledEnabledOnBuild) content.value.config.airPlayDisabledEnabledOnBuild = config.airPlayDisabledEnabledOnBuild;
-	if (content.value?.config?.preferRichWebVTTOverImageBasedSubtitle) content.value.config.preferRichWebVTTOverImageBasedSubtitle = config.preferRichWebVTTOverImageBasedSubtitle;
-	if (content.value?.config?.reuseAVPlayerEnabledOnBuild) content.value.config.reuseAVPlayerEnabledOnBuild = config.reuseAVPlayerEnabledOnBuild;
-	if (content.value?.config?.nfplayerReduxEnabledOnBuild) content.value.config.nfplayerReduxEnabledOnBuild = config.nfplayerReduxEnabledOnBuild;
+	console.log('before' + content.value.config.allowWidevinePlayback);
+	if (content.value?.geolocation?.country) content.value.geolocation.country = geolocation.country ? geolocation.country : content.value.geolocation.country;
+	if (content.value?.config?.allowWidevinePlayback) content.value.config.allowWidevinePlayback = config.allowWidevinePlayback ? config.allowWidevinePlayback : content.value.config.allowWidevinePlayback;
+	console.log('after' + content.value.config.allowWidevinePlayback);
+	if (content.value?.config?.airPlayDisabledEnabledOnBuild) content.value.config.airPlayDisabledEnabledOnBuild = config.airPlayDisabledEnabledOnBuild ? config.airPlayDisabledEnabledOnBuild : content.value.config.airPlayDisabledEnabledOnBuild;
+	if (content.value?.config?.preferRichWebVTTOverImageBasedSubtitle) content.value.config.preferRichWebVTTOverImageBasedSubtitle = config.preferRichWebVTTOverImageBasedSubtitle ? config.preferRichWebVTTOverImageBasedSubtitle : content.value.config.preferRichWebVTTOverImageBasedSubtitle;
+	if (content.value?.config?.reuseAVPlayerEnabledOnBuild) content.value.config.reuseAVPlayerEnabledOnBuild = config.reuseAVPlayerEnabledOnBuild ? config.reuseAVPlayerEnabledOnBuild : content.value.config.reuseAVPlayerEnabledOnBuild;
+	if (content.value?.config?.nfplayerReduxEnabledOnBuild) content.value.config.nfplayerReduxEnabledOnBuild = config.nfplayerReduxEnabledOnBuild ? config.nfplayerReduxEnabledOnBuild : content.value.config.nfplayerReduxEnabledOnBuild;
 	body = JSON.stringify(content);
 	done({ body });
-};
+}
+
+//Check IP Status
+else if (url.search(path2) != -1) {
+	console.log(url);
+	let content = JSON.parse(body);
+	console.log('before' + content.ctx);
+	if (content.ctx?.hasUser) content.ctx.hasUser = ctx.hasUser ? ctx.hasUser : content.ctx.hasUser;
+	console.log('after' + content.ctx);
+	body = JSON.stringify(content);
+	done({ body });
+}
+
+else done();
