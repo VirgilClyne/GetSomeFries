0066a8f492e5cb1b4748151e1b0cf710fa570188
Virgil Clyne <Virgil@nanocat.me>
2022-06-14 21:05:23 +0800

    Function: Update getENV()
    Update getENV.full.js
    Update getENV.js
    Update getENV.min.js

--- function/getENV/getENV.js
+++ function/getENV/getENV.js
@@ -1,5 +1,6 @@
 /**
  * Get Environment Variables
+ * @link https://github.com/VirgilClyne/VirgilClyne/blob/main/function/getENV/getENV.js
  * @author VirgilClyne
  * @param {String} key - Persistent Store Key
  * @param {String} name - Platform Name
@@ -13,25 +14,27 @@ async function getENV(key, name, database) {
 	// BoxJs的清空操作返回假值空字符串, 逻辑或操作符会在左侧操作数为假值时返回右侧操作数。
 	let BoxJs = $.getjson(key, database);
 	//$.log(`🚧 ${$.name}, Get Environment Variables`, `BoxJs类型: ${typeof BoxJs}`, `BoxJs内容: ${JSON.stringify(BoxJs)}`, "");
-	/***************** Settings *****************/
-	let Settings = BoxJs?.[name]?.Settings || database?.[name]?.Settings || database?.Default?.Settings;
-	//$.log(`🎉 ${$.name}, Get Environment Variables`, `Settings: ${typeof Settings}`, `Settings内容: ${JSON.stringify(Settings)}`, "");
-	let Configs = BoxJs?.[name]?.Configs || database?.[name]?.Configs || database?.Default?.Configs;
-	let Caches = BoxJs?.[name]?.Caches || undefined;
-	if (typeof Caches === "string") Caches = JSON.parse(Caches)
 	/***************** Argument *****************/
-	if (typeof $argument != "undefined") {
-		if ($argument) {
+	let Argument = {};
+	if (typeof $argument !== "undefined"){
+		if (Boolean($argument)) {
 			//$.log(`🎉 ${$.name}, $Argument`);
 			let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
-			//$.log(JSON.stringify($argument));
-			let newArg = {};
-			for (var item in arg) setPath(newArg, item, arg[item]);
-			//$.log(JSON.stringify(newArg));
-			Object.assign(Settings, newArg);
-		}
-		function setPath(object, path, value) {path.split(".").reduce((o, p, i) => o[p] = path.split(".").length === ++i ? value : o[p] || {}, object)}
+			//$.log(JSON.stringify(arg));
+			for (let item in arg) setPath(Argument, item, arg[item]);
+			//$.log(JSON.stringify(Argument));
+		};
 	};
+	//$.log(`🎉 ${$.name}, Get Environment Variables`, `Argument类型: ${typeof Argument}`, `Argument内容: ${JSON.stringify(Argument)}`, "");
+	/***************** Settings *****************/
+	let Settings = { ...database?.Default?.Settings, ...database?.[name]?.Settings, ...BoxJs?.[name]?.Settings, ...Argument };
 	//$.log(`🎉 ${$.name}, Get Environment Variables`, `Settings: ${typeof Settings}`, `Settings内容: ${JSON.stringify(Settings)}`, "");
-	return { Settings, Caches, Configs }
+	let Configs = { ...database?.Default?.Configs, ...database?.[name]?.Configs, ...BoxJs?.[name]?.Configs };
+	//$.log(`🎉 ${$.name}, Get Environment Variables`, `Configs: ${typeof Configs}`, `Config内容: ${JSON.stringify(Configs)}`, "");
+	let Caches = BoxJs?.[name]?.Caches || undefined;
+	if (typeof Caches === "string") Caches = JSON.parse(Caches)
+	//$.log(`🎉 ${$.name}, Get Environment Variables`, `Caches: ${typeof Caches}`, `Caches内容: ${JSON.stringify(Caches)}`, "");
+	return { Settings, Caches, Configs };
+	/***************** setPath *****************/
+	function setPath(object, path, value) { path.split(".").reduce((o, p, i) => o[p] = path.split(".").length === ++i ? value : o[p] || {}, object) }
 };
